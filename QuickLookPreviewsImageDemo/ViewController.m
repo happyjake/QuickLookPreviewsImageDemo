74b32fc5e7e1f65716ca1d2f4237226e35efde35
happyjake <happyjake@gmail.com>
2015-11-29 22:40:44 +0800

    preview images resources

--- QuickLookPreviewsImageDemo/ViewController.m
+++ QuickLookPreviewsImageDemo/ViewController.m
@@ -7,18 +7,47 @@
 //
 
 #import "ViewController.h"
+#import <Quartz/Quartz.h>
 
-@implementation ViewController
+@interface WCQLPreviewItem : NSObject  <QLPreviewItem>
+@property(retain) NSURL* itemUrl;
+- (NSString *)previewItemTitle;
+- (NSURL *)previewItemURL;
+@end
+
+
+@implementation WCQLPreviewItem
+
+- (NSString *)previewItemTitle {
+    return self.itemUrl.absoluteString;
+}
+- (NSURL *)previewItemURL {
+    return self.itemUrl;
+}
+
+@end
+
+@implementation ViewController {
+    QLPreviewView *qlView;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    
     // Do any additional setup after loading the view.
+    qlView = [[QLPreviewView alloc] initWithFrame:self.view.bounds style:QLPreviewViewStyleNormal];
+    qlView.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable | NSViewMinXMargin | NSViewMinYMargin;
+    
+    WCQLPreviewItem* previewItem = [WCQLPreviewItem new];
+    previewItem.itemUrl = [[NSBundle mainBundle] URLForImageResource:@"IMG_3925"];
+    qlView.previewItem = previewItem;
+    
+    [self.view addSubview:qlView];
 }
 
 - (void)setRepresentedObject:(id)representedObject {
     [super setRepresentedObject:representedObject];
-
+    
     // Update the view, if already loaded.
 }
 
